88002b5c5688894d0893178c7e6643bb11648fcd
Robson Ferreira <robsonldfer@gmail.com>
2026-08-30 19:17:56 -0300

    Update register.php

--- Sistema/pages/register.php
+++ Sistema/pages/register.php
@@ -3,5 +3,12 @@
     $message="";
     $toastClass="";
 
-    if $_SERVER["REQUEST_METHOD"] == "POST")`
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        //check email already exists
+        
+    }
 ?> 
