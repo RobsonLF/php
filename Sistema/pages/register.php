5f1ccce8a41e2a1bad7dba8f6ba1b4dcafa02fdc
Robson Ferreira <robsonldfer@gmail.com>
2026-08-30 23:16:00 -0300

    Update register.php

--- Sistema/pages/register.php
+++ Sistema/pages/register.php
@@ -9,6 +9,12 @@
         $password = $_POST['password'];
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         //check email already exists
-        
+        $checkEmailStmt = $conn->prepare("SELECT email FROM userdata WHERE email = ?");
+        $checkEmailStmt->bind_param("s", $email);
+        $checkEmailStmt->execute(); 
+        $checkEmailStmt->store_result();
+        if ($checkEmailStmt->num_rows() > 0){
+            $message = "Email ID  already existes"
+        }
     }
 ?> 
